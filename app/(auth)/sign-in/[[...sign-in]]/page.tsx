66fd5d209e9da41e71d05c2dd4ed54d408c94112
Omar Hisham <omar.hisham0597@gmail.com>
2024-10-09 20:03:43 +0300

    Update page.tsx

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -4,10 +4,4 @@ const SignInPage = () => {
   return <SignIn />
 }
 
-export default SignInPageimport { SignIn } from '@clerk/nextjs'
-
-const SignInPage = () => {
-  return <SignIn />
-}
-
 export default SignInPage
